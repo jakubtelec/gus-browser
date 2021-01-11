fb47f78b28ec59a6db35c227cbff21427e1bf60a
Jakub Telec <jakub.telec@gmail.com>
2021-01-11 09:56:23 +0100

    Update helpers.js

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -83,12 +83,3 @@ const pathGetters = [
 
 export const chartDefsToPaths = (chartDefs) =>
   chartDefs.map((def) => pathGetters.map((getter) => getter(def)));
-
-export const getDateOfISOWeek = (w, y) => {
-  var simple = new Date(y, 0, 1 + (w - 1) * 7);
-  var dow = simple.getDay();
-  var ISOweekStart = simple;
-  if (dow <= 4) ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
-  else ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
-  return ISOweekStart;
-};
